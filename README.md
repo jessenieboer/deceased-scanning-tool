a9a9d9f2855e69f7f2afe1e320f5f45fd4e9ece3
Si Research <93379837+SiResearch@users.noreply.github.com>
2021-10-29 18:22:03 +1100

    Initial commit, version 1.0, very rough

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# deceased-scanning-tool
-Deceased person searching tool
+# Deceased Person Search Tool
+
+By [@SiWiFi](https://t.me/SiWiFi)
+
+I have not documented this much yet due to lack of time, sorry. May get to it if there's enough interest.
+
+Built using Vue JS and the vue-cli tool. Runs in-browser.
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,230 @@
+<template>
+  <div id="app">
+
+    <h2>Deceased Person Search Tool</h2>
+
+    <p><small>Version 1.0. Caution: This is still very rough and there may be bugs.</small></p>
+
+    <script type="application/javascript" src="https://cdn.jsdelivr.net/npm/xlsx@0.17.3/dist/xlsx.full.min.js"></script>
+
+    <div v-if="loading">
+      Loading spreadsheet...
+    </div>
+
+    <div v-else-if="!data">
+      Drop an XLS file here to start.
+    </div>
+
+    <div v-else>
+      <p>Found {{ data.length }} records.</p>
+      <p>
+        Here are the ones who voted, and are older than 
+        <select v-model="ageLimit">
+          <option v-for="age in ageRange" :key="age" :value="age">{{ age }}</option>
+        </select>.
+        Found {{ filteredList.length }} of those.
+      </p>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Unique Voter ID's</th>
+            <th>First Name</th>
+            <th>Middle Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Birthday</th>
+            <th>Address</th>
+            <th>Precinct</th>
+            <th>Search</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="row in filteredList.slice(0,1000)" :key="row['Unique Voter ID\'s']">
+            <td>{{ row['Unique Voter ID\'s'] }}</td>
+            <td>{{ row['Name'] }}</td>
+            <td>{{ row['Name_1'] }}</td>
+            <td>{{ row['Name_2'] }}</td>
+            <td>{{ age(row) }}</td>
+            <td>{{ row['Birthday'] }}</td>
+            <td>{{ row['Address'] }}</td>
+            <td>{{ row['Precinct'] }}</td>
+            <td>
+              <a :href="findAGraveUrl(row)" target="fag">FindAGrave</a>&nbsp;
+              <a :href="ancestryUrl(row)" target="ancestry">Ancestry</a>&nbsp;
+              <a href="#" @click.prevent="openBothWindows(row)">Both</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      Max of 1000 records shown here.
+
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data: () => ({
+    loading: false,
+    ssheet: null,
+    data: null,
+    ageLimit: 80,
+    votedList: null,
+  }),
+  components: {
+    // HelloWorld
+  },
+  watch: {
+    data() {
+      this.votedList = this.data && this.data
+        .filter(item => item['Voted_1'] == 'Yes' || item['Voted?_1'] == 'Yes')
+        .map(item => {
+          // Standardize rows
+          item.age = this.age(item)
+          return item;
+        });
+    },
+  },
+  computed: {
+    filteredList() {
+      return this.votedList && this.votedList
+        .filter(item => 
+            item.age > this.ageLimit
+        )
+        .sort((a,b) => b.age - a.age);
+    },
+    ageRange() {
+      return Array.from(Array(120).keys());
+    }
+  },
+  methods: {
+    age(record) {
+      if (record['Birthday'])
+        return 2021 - record['Birthday'].split('/')[2];
+      else if (record['Birthdate'])
+        return 2021 - record['Birthdate'].split('/')[2];
+    },    
+    openBothWindows(item) {
+      window.open(this.ancestryUrl(item), 'ancestry');
+      window.open(this.findAGraveUrl(item), 'fag');
+    },
+    ancestryUrl(item) {
+      return 'https://www.ancestry.com/search/categories/bmd_death/?name=' +
+      encodeURI(item.Name + ' ' + item.Name_1) +
+      '_' +
+      encodeURI(item.Name_2) +
+      '&event=_' +
+      // 'columbia-boone-'
+      'missouri-usa' +
+      '&birth=' +
+      (item.Birthday || item.Birthdate || '').split('/')[2] +
+      '&birth_x=0-0-0&name_x=1_1';
+    },
+    findAGraveUrl(item) {
+      item;
+      return 'https://www.findagrave.com/memorial/search?firstname=' +
+      encodeURI(item.Name) +
+      '&middlename=' +
+      encodeURI(item.Name_1) +
+      '&lastname=' +
+      encodeURI(item.Name_2) +
+      '&birthyear=' +
+      (item.Birthday || item.Birthdate || '').split('/')[2] +
+      '&birthyearfilter=' +
+      '&deathyear=&deathyearfilter=&location=&locationId=&memorialid=&mcid=&linkedToName=&datefilter=&orderby=r&plot=';
+    },
+  },
+  mounted() {
+    console.log('mounted');
+    var $this = this;
+    window.VueApp = this;
+
+    var holder = document.querySelector('body');
+    
+    holder.ondragover = function (e) { 
+      e.preventDefault();
+      this.className = 'hover'; return false; 
+    };
+    // holder.ondragend = function () { this.className = ''; return false; };
+    holder.addEventListener('drop', async function (e) {
+      
+      // console.log('file dropped');
+      // console.log(e);
+      e.preventDefault();
+
+      // $this.data = null;
+      $this.loading = true;
+
+      $this.$nextTick(async function() {
+
+        const f = e.dataTransfer.files[0];
+        const data = await f.arrayBuffer();
+        const workbook = window.XLSX.read(data);
+        var ssheet = workbook.Sheets[workbook.SheetNames[0]];
+        $this.data = window.XLSX.utils.sheet_to_json(ssheet, {range:1});
+        $this.loading = false;
+
+        // delete f;
+        // delete data;
+        // delete workbook;
+        // delete ssheet;
+
+      });
+
+
+      // var headerRows = 2;
+
+
+      // var file = e.dataTransfer.files[0],
+      //     reader = new FileReader();
+      // reader.onload = function (event) {
+      //   console.log(event.target);
+      //   var sheet = window.XLSX.read(event.target.result);
+        
+      //   // holder.style.background = 'url(' + event.target.result + ') no-repeat center';
+      // };
+      // console.log(file);
+      // reader.readAsDataURL(file);
+
+      // return false;
+    });
+  }
+}
+</script>
+
+<style>
+html, body {
+	height: 100%;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+table {
+  border-collapse: collapse;
+}
+tr:hover td {
+  background-color: #f2f2f2;
+}
+td, th {
+  border: 1px solid #ccc;
+  font-size: 90%;
+  padding: 2px 4px;
+}
+th {
+  font-weight: bold;
+  background-color: #eee;
+  font-size: 77%;
+}
+</style>
